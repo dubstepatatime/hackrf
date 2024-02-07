a2834184f7963a58d580ad90f1c373b6207b0fe2
dubstepatatime <158757545+dubstepatatime@users.noreply.github.com>
2024-02-07 03:42:10 -0600

    jyf

---
+++ Untitled-1.bat
@@ -0,0 +1,7 @@
+c:\hackrf\host\build> cmake ../ -G "Visual Studio 14 2015 Win64" \
+-DLIBUSB_INCLUDE_DIR=c:\libusb-1.0.21\libusb \
+-DLIBUSB_LIBRARIES=c:\libusb-1.0.21\MS64\dll\lib\libusb-1.0.lib \
+-DTHREADS_PTHREADS_INCLUDE_DIR=c:\pthreads-w32-2-9-1-release\Pre-built.2\include \
+-DTHREADS_PTHREADS_WIN32_LIBRARY=c:\pthreads-w32-2-9-1-release\Pre-built.2\lib\x64\pthreadVC2.lib \
+-DFFTW_INCLUDES=C:\fftw-3.3.5-dll64 \
+-DFFTW_LIBRARIES=C:\fftw-3.3.5-dll64\libfftw3f-3.lib
